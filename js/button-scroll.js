49a02bc737929c119d29742d4e873d839a401bcd
Andreiskate2 <andrei.ungur@mail.mcgill.ca>
2017-08-09 21:18:10 -0400

    Add body + animations
    Only things missing for a minimal version is the language settings for content + calendar with something to automatically send an email + handle payments when a date is picked.

--- js/button-scroll.js
+++ js/button-scroll.js
@@ -1,5 +1,31 @@
+$(document).ready(function(){
+  $(".header").click(display());
+  $(".About").hover(function(){$("#slider").css("margin-left","0")});
+  $(".Images").hover(function(){$("#slider").css("margin-left","29%")});
+  $(".Reservations").hover(function(){$("#slider").css("margin-left","62%")});
+});
+function display(){
+  $(".About").click(function(){
+    $("#slider").css("margin-left","0");
+    $("#AboutContent").fadeIn();
+    $("#ImagesContent, #ReservationsContent").hide();
+  });
+  $(".Images").click(function(){
+    $("#slider").css("margin-left","29%");
+    $("#ImagesContent").fadeIn();
+    $("#AboutContent, #ReservationsContent").hide();
+  });
+  $(".Reservations").click(function(){
+    $("#slider").css("margin-left","62%");
+    $("#ReservationsContent").fadeIn();
+    $("#AboutContent, #ImagesContent").hide();
+  });
+}
+
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
+    display();
+    delay(1000);
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
